1972108ed024d86dde88457b7cab71881b56b7a8
picflix <picflixofficial@gmail.com>
2025-09-21 22:54:46 +0530

    Add files via upload

---
+++ _next/static/e3GOvxw73s0V3d1hoqARu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cff3e69d-d62f397ab335c41f.js","static/chunks/887-17ae94c0fe02f9d0.js","static/chunks/pages/index-d60c1ae891fda876.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
